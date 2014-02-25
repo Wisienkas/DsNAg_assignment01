53ce31bb92fe8bae849b23cff8c8a730c14254a3
Nikolaj Reibke <nire@tv2.dk>
2014-02-25 21:59:15 +0100

    Added skeleton for part 2

---
+++ src/dk/sdu/nirei12/dictionary/Dict.java
@@ -0,0 +1,7 @@
+package dk.sdu.nirei12.dictionary;
+
+public interface Dict {
+	public void insert(int k);
+	public int[] orderedTraversal();
+	public boolean search(int k);
+}
---
+++ src/dk/sdu/nirei12/dictionary/DictBinTree.java
@@ -0,0 +1,27 @@
+package dk.sdu.nirei12.dictionary;
+
+public class DictBinTree implements Dict {
+	
+	public DictBinTree(){
+		
+	}
+	
+	@Override
+	public void insert(int k) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public int[] orderedTraversal() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean search(int k) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
